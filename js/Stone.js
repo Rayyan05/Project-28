fa487d89ba653a9aed98086827058ec23b531567
Rayyan05 <75606877+Rayyan05@users.noreply.github.com>
2021-01-24 15:52:13 +0530

    Add files via upload

---
+++ js/Stone.js
@@ -0,0 +1,24 @@
+class Stone{
+    constructor(x,y,width,height){
+        var options = {
+           
+            'restitution':1.0
+        }
+
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        this.width = width
+        this.height = height
+        this.image = loadImage("images/stone.png")
+        World.add(world,this.body)
+    }
+
+    display(){
+        var pos = this.body.position
+        var angle = this.body.angle
+
+        push();
+        imageMode(CENTER)
+        image(this.image,pos.x,pos.y,this.width,this.height)
+        pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2 =new mango(1100,200,30);
+	mango3 = new mango(1000,200,30);
+	mango4 = new mango(1000,100,30);
+	mango5 = new mango(1200,150,30);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+
+	stone1 = new Stone(215,400,60,60)
+
+	sling1 = new Slingshot(stone1.body,{x:215,y:400})
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  stone1.display();
+  sling1.display();
+
+  groundObject.display();
+}
+
+function mouseDragged(){
+	Matter.Body.setPosition(stone1.body,{x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+	sling1.fly();
+}
+
+
+
+
